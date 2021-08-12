1bba848a84841dbbc025c5bfeeb012bcb8b6b507
mkizka <g.mkizka@gmail.com>
2021-08-12 23:28:35 +0900

    閉じるボタン追加

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -12,12 +12,22 @@ function needPositive(value: string, initialValue: number) {
   return parsed >= 0 ? parsed : initialValue;
 }
 
+function createCloseButton(onClose: () => void) {
+  const button = document.createElement("button");
+  button.type = "button";
+  button.textContent = "x";
+  button.classList.add("komado-close");
+  button.addEventListener("click", onClose);
+  return button;
+}
+
 async function main() {
   const storage = await browser.storage.sync.get(["playerWidth"]);
   const playerWidth = needPositive(storage.playerWidth, 480);
   const playerHeight = (playerWidth * 9) / 16;
   insertStyle(`
-  .komado-minimize {
+  [data-komado-state="minimized"] {
+    overflow: initial !important;
     position: fixed !important;
     right: 20px !important;
     bottom: 20px !important;
@@ -25,25 +35,37 @@ async function main() {
     width: ${playerWidth}px !important;
     z-index: 10000 !important;
   }
-  .komado-minimize video {
+  [data-komado-state="minimized"] video {
     height: ${playerHeight}px !important;
     width: ${playerWidth}px !important;
   }
-  .komado-minimize .ytp-right-controls {
+  [data-komado-state="minimized"] .ytp-right-controls {
     display:none !important;
+  }
+  [data-komado-state="minimized"] .komado-close {
+    display: block;
+    position: relative;
+    top: -2rem;
+    margin-left: auto;
   }
   `);
   setInterval(() => {
     const player = document.querySelector<HTMLDivElement>("#movie_player");
+    const closeButton = createCloseButton(() => {
+      player.dataset.komadoState = "closed";
+    });
     if (player == null) return;
-    const minimized = player.classList.contains("komado-minimize");
     const shouldMinimize =
       window.pageYOffset > player.parentElement.offsetHeight * 0.75;
-    if (!minimized && shouldMinimize) {
-      player.classList.add("komado-minimize");
+    if (shouldMinimize && player.dataset.komadoState == "ready") {
+      player.insertAdjacentElement("beforeend", closeButton);
+      player.dataset.komadoState = "minimized";
     }
-    if (minimized && !shouldMinimize) {
-      player.classList.remove("komado-minimize");
+    if (!shouldMinimize) {
+      if (player.dataset.komadoState == "minimized") {
+        closeButton.remove();
+      }
+      player.dataset.komadoState = "ready";
     }
     window.dispatchEvent(new Event("resize"));
   }, 100);
