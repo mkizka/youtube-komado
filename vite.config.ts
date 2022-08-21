c1781ae8f7786b692bab9d42e813b88a7a98edaa
mkizka <g.mkizka@gmail.com>
2022-08-21 22:55:23 +0900

    rollup-plugin-zip追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
+import zip from "rollup-plugin-zip";
 import manifest from "./src/manifest.json";
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), zip()],
 });
