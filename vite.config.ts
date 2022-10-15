8c67b668e21e399ebe74c87f38070f14af521e6c
mkizka <g.mkizka@gmail.com>
2022-10-15 17:15:50 +0900

    manifest.json移動

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import zip from "rollup-plugin-zip";
-import manifest from "./src/manifest.json";
+import manifest from "./manifest.json";
 
 export default defineConfig({
   plugins: [react(), crx({ manifest }), zip()],
