3f871c59235eea386bbd18fb7572e005a6ae880c
mkizka <g.mkizka@gmail.com>
2025-12-17 22:06:40 +0900

    remove all

--- README.md
+++
@@ -1,7 +0,0 @@
-# youtube-komado
-
-YouTube動画を小窓で表示するブラウザ拡張機能
-
-Chrome Web Store: https://chrome.google.com/webstore/detail/njnjbpmcgffckgidchbadfbkeghnffnh
-
-<img src="screenshot.png" width="400">
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
-import zip from "rollup-plugin-zip";
-import manifest from "./manifest.json";
-
-export default defineConfig({
-  plugins: [react(), crx({ manifest }), zip()],
-});
